f2a42759bcaeee9b1929ef43c51d11c3174d1600
Xutop <906344661@qq.com>
2016-11-10 11:26:51 +0800

    add unittest

--- tests/test_networkhelper.py
+++ tests/test_networkhelper.py
@@ -11,3 +11,14 @@ class TestGlobalPooling(unittest.TestCase):
         gp = N.GlobalPooling()
         # TODO
         self.assertEqual(1,1)
+        
+class TestConv2d(unittest.TestCase):
+    
+    def test_conv2d(self):
+        x = np.random.rando(100, 1, 28, 28)
+        conv2d = N.Conv2d(filter_size=(3,3), feature_map_multiplier=20)
+        y = conv2d.forward(x)
+        self.assertEqual(y, (100, 20, 28, 28))
+        
+if __name__ == " __main__":
+    unittest.main()
