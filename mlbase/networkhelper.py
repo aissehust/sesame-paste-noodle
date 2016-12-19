249850202ba62a90214c937b351ac222024ac4c9
Xutop <906344661@qq.com>
2016-12-19 18:21:22 +0800

    dropout

--- mlbase/networkhelper.py
+++ mlbase/networkhelper.py
@@ -788,6 +788,81 @@ class BatchNormalization(Layer):
         ret.loadFromObjMap(obj_dict)
         return ret
 
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+class Dropout(Layer):
+
+    debugname = 'dropout'
+    LayerTypeName = 'dropout'
+    yaml_tag = u'!dropout'
+    
+    def __init__(self, p=0.5, rescale=True, shared_axes=()):
+        super(Dropout, self).__init__()
+        self._srng = RandomStreams(np.random.randint(1, 2147462579))
+        self.p = p
+        self.rescale = rescale
+        self.shared_axes = tuple(shared_axes)
+
+    def getpara(self):
+        return []
+        
+    def forward(self, inputtensor, deterministic=False):
+        
+        if deterministic or self.p == 0:
+            return inputtensor
+
+        else:
+            x = inputtensor[0]
+            # Using theano constant to prevent upcasting
+            one = T.constant(1)
+
+            retain_prob = one - self.p
+            if self.rescale:
+                x /= retain_prob
+
+            mask_shape = x.shape
+
+            # apply dropout, respecting shared axes
+            if self.shared_axes:
+                shared_axes = tuple(a if a >= 0 else a + x.ndim
+                                    for a in self.shared_axes)
+                mask_shape = tuple(1 if a in shared_axes else s
+                                   for a, s in enumerate(mask_shape))
+            mask = self._srng.binomial(mask_shape, p=retain_prob,
+                                       dtype=x.dtype)
+            if self.shared_axes:
+                bcast = tuple(bool(s == 1) for s in mask_shape)
+                mask = T.patternbroadcast(mask, bcast)
+            x = x * mask
+            return (x, )
+        
+    def forwardSize(self, inputsize):
+        return inputsize
+    
+    def fillToObjMap(self):
+        objDict = super(Dropout, self).fillToObjMap()
+        objDict['probability'] = self.p
+        objDict['rescale'] = self.rescale
+
+        return objDict
+
+    def loadFromObjMap(self, tmap):
+        super(Dropout, self).loadFromObjMap(tmap)
+        self.inputFeature = tmap['probability']
+        self.outputFeature = tmap['rescale']
+
+    @classmethod
+    def to_yaml(cls, dumper, data):
+        obj_dict = data.fillToObjMap()
+        node = dumper.represent_mapping(Dropout.yaml_tag, obj_dict)
+        return node
+
+    @classmethod
+    def from_yaml(cls, loader, node):
+        obj_dict = loader.construct_mapping(node)
+        ret = Dropout(input_feature=obj_dict['probability'],
+                       output_feature=obj_dict['rescale'])
+        ret.loadFromObjMap(obj_dict)
+        return ret
 
 class Network(learner.SupervisedLearner):
     """
