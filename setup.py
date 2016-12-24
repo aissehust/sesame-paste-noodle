3d0ed220501e6ecd0f27fbb2d76339e1ad848289
yguan <pipehappy@pipehappy.me>
2016-12-24 22:15:02 +0800

    add setup.up file for packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages
+from setuptools import setup
+
+install_requires = [
+    'numpy',
+]
+
+setup(
+    name="TheFramework",
+    version="0.0.1",
+    description="A nn lib",
+    packages=find_packages(),
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=install_requires,
+)
