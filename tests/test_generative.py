920e813d1100697045a8593a1fc3f96e658f93e2
yguan <yguan@guanshankou.cn>
2017-02-07 20:49:35 +0800

    fix build

--- tests/test_generative.py
+++ tests/test_generative.py
@@ -4,6 +4,7 @@ import numpy as np
 import mlbase.network as N
 import mlbase.layers.layer as layer
 import unittest
+from mlbase.layers import GlobalPooling
 
 class TestGenerative(unittest.TestCase):
 
@@ -11,12 +12,12 @@ class TestGenerative(unittest.TestCase):
 
     def test_upconv(self):
         x = np.random.randn(256, 32, 28, 28)
-        gp = layer.GlobalPooling()
+        gp = GlobalPooling()
         # TODO
         self.assertEqual(1,1)
 
     def test_upconv1(self):
         x = np.random.randn(256, 32, 28, 28)
-        gp = layer.GlobalPooling()
+        gp = GlobalPooling()
         # TODO
         self.assertEqual(1,1)
